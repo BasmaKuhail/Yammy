f0a601433a72c29032d9aa6d80894ca5d05c3b39
BasmaKuahil <basmakuahil2003@gmail.com>
2019-12-28 10:34:09 +0200

    new style for screen1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img className='bigimg' src={'https://images.pexels.com/photos/54455/cook-food-kitchen-eat-54455.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'}/>
+
+      <div  class="container">
+      <img className='bigimg' src={'https://images.pexels.com/photos/616404/pexels-photo-616404.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'}/>
+      <div class="overlay">about us about usabout usabout usabout usabout usabout usabout usabout usabout us
+      about usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usabout usab
+      out usabout usabout usabout usabout usabout usabout usabout usabout us</div>
+      </div>
+
      <div className="Top-left"> 
        <a href="https://www.facebook.com/basmakuhail2003">
           <img className='image' src={'https://www.facebook.com/images/fb_icon_325x325.png'} />
@@ -17,16 +24,26 @@ function App() {
         <a href="https://www.facebook.com/basmakuhail2003">
         <img className='image' src={'https://static01.nyt.com/images/2014/08/10/magazine/10wmt/10wmt-articleLarge-v4.jpg?quality=75&auto=webp&disable=upscale'} />
         </a>
-        <div className='top-right'>
+       
         <button className="button">sign up</button>
         </div>
-        </div>
-        <h1 className='centered'> Yammy!</h1>
-        <div >
+        
+        <div className='centered'>
+        <h1> Yammy!</h1>
         <input className='input' placeholder="  serch for meal"></input>
         <img className='image' src={'https://img.icons8.com/pastel-glyph/2x/search.png'} />
+        <div >
+          <button className='filter'>potato</button>
+          <button className='filter'>tomato</button>
+          <button className='filter'>onion</button>
+          <button className='filter'>meat</button>
+          <button className='filter'>rock</button>
+          <button className='filter'>potato</button>
+
+        </div>
         </div>
 
+        
 
 
       
