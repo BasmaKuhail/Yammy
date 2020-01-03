9a0dfa269fb63b519a98b66c885f8dfea7e02793
BasmaKuahil <basmakuahil2003@gmail.com>
2020-01-03 12:26:45 +0200

    firebase

---
+++ src/Components/Home/Cards.js
@@ -0,0 +1,252 @@
+import React , {Component}from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+
+
+
+class Cards extends Component{
+    
+    render(){
+        const useStyles = makeStyles(theme => ({
+            card: {
+              maxWidth: 600,
+            },
+            media: {
+              width:350,
+              height: 30,
+              paddingTop: '65.25%',
+            },
+            expand: {
+              transform: 'rotate(0deg)',
+              marginLeft: 'auto',
+              transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+              }),
+            },
+            expandOpen: {
+              transform: 'rotate(180deg)',
+            },
+            avatar: {
+              backgroundColor: red[500],
+            },
+          }));
+          
+          const classes = useStyles();
+          const [expanded, setExpanded] = React.useState(false);
+        
+          const handleExpandClick = () => {
+            setExpanded(!expanded);
+          };
+        
+        return(
+            <div className='cards'>
+                <div className='cards'>
+                    <Card className={classes.card}>
+                    <CardHeader
+                        avatar={
+                        <Avatar aria-label="recipe" className={classes.avatar}>
+                            B
+                        </Avatar>
+                        }
+                        
+                        title="Pizza"
+                        subheader="By: mahmood"
+                    />
+
+                    <CardMedia
+                        className={classes.media}
+                        image="https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                        Can make your day!
+                        </Typography>
+                    </CardContent>
+                
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                        </IconButton>
+
+                    
+                        <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                        >
+                    
+                        <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                        <Typography paragraph>
+                            recipe:
+
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        </CardContent>
+                    
+                    </Collapse>
+                    </Card>
+                </div>
+                <div className='cards'>
+                <Card className={classes.card}>
+                    <CardHeader
+                        avatar={
+                        <Avatar aria-label="recipe" className={classes.avatar}>
+                            R
+                        </Avatar>
+                        }
+                        
+                        title="Pizza"
+                        subheader="By: mahmood"
+                    />
+
+                    <CardMedia
+                        className={classes.media}
+                        image="https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                        Can make your day!
+                        </Typography>
+                    </CardContent>
+                
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                        </IconButton>
+
+                    
+                        <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                        >
+                    
+                        <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                        <Typography paragraph>
+                            recipe:
+
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        </CardContent>
+                    
+                    </Collapse>
+                    </Card>
+                </div>
+                <div className='cards'>
+                <Card className={classes.card}>
+                    <CardHeader
+                        avatar={
+                        <Avatar aria-label="recipe" className={classes.avatar}>
+                            R
+                        </Avatar>
+                        }
+                        
+                        title="Pizza"
+                        subheader="By: mahmood"
+                    />
+
+                    <CardMedia
+                        className={classes.media}
+                        image="https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                    />
+                
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                        Can make your day!
+                        </Typography>
+                    </CardContent>
+                
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                        </IconButton>
+
+                    
+                        <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                        >
+                    
+                        <ExpandMoreIcon />
+                        </IconButton>
+                    </CardActions>
+                
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                        <Typography paragraph>
+                            recipe:
+
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        <Typography>
+                        1. put it in oven
+                        </Typography>
+                        </CardContent>
+                    
+                    </Collapse>
+                    </Card>
+                </div>    
+            </div>
+
+        )
+    }
+
+}
+
+export default Cards;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXX8hp81_yiptvIDyKch-i_F39nBe4FcM",
+    authDomain: "yammy-c473e.firebaseapp.com",
+    databaseURL: "https://yammy-c473e.firebaseio.com",
+    projectId: "yammy-c473e",
+    storageBucket: "yammy-c473e.appspot.com",
+    messagingSenderId: "635335262399",
+    appId: "1:635335262399:web:84214970715909beb2460a",
+    measurementId: "G-Q1J13KHJYY"
+  };
+
+  
+// const fire= 
+firebase.initializeApp(firebaseConfig);
+// export default fire; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
